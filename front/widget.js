74fcdf56f3cdff9da6496338bdb3dad50a43d28f
krstkaan <k.kaanakbulak@gmail.com>
2024-05-15 11:45:48 +0300

    Front

---
+++ front/widget.js
@@ -0,0 +1,18 @@
+<script type="text/javascript">
+
+$(function(){
+typeof $.fn.paraceviriciWidget == "function" && 
+$("#W8780").paraceviriciWidget({
+	widget:"boxline",
+	wData:{
+		category:0,
+		currency:"USD-EUR"
+	},
+	wSize:{
+		wWidth:200,
+		wHeight:20
+	}
+})
+});
+
+</script>
